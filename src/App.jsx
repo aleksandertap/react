d2bafba688cf274646076c1231b59c25124e04b0
aleksander <aleksander@aleksander.aleksander>
2025-01-23 13:16:36 +0200

    propside lisamine

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import ExpenseItem from "./components/ExpenseItem.jsx";
 
 const App = () => {
+  const expenses = [
+    { date: new Date(2025, 0, 23), title: "New Book", price: 30.99 },
+    { date: new Date(2025, 0, 23), title: "New Jeans", price: 99.99 },
+  ];
+
   return (
     <div className="App">
-      <ExpenseItem />
+      <ExpenseItem data={expenses[0]} />
+      <ExpenseItem data={expenses[1]} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
